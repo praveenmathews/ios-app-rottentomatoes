ba4379491418d112b740726379d8a295424e8a5b
Praveen Idiculla Mathews <praveenmathews88@yahoo.in>
2015-01-26 19:04:38 -0800

    Git Commit

---
+++ Rotten Tomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rotten Tomatoes
+//
+//  Created by Praveen Idiculla Mathews on 1/20/15.
+//  Copyright (c) 2015 Praveen Idiculla Mathews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
